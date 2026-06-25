b71bf9cee745e83ae229d205c780b783513b46f7
Tobias <Tobiassenft@hotmail.com>
2026-06-25 16:48:48 +0200

    Added functionality for getting stock data, and added a .csv for the data for the data to be stored in

--- loadData.py
+++ loadData.py
@@ -1,1 +1,7 @@
 import yfinance as yf
+import pandas as pd
+
+
+# pulling data from API putting it into csv for easier use
+data = pd.DataFrame(yf.download(["AAPL", "MSFT", "GOOGL"],period="1y"))
+data.to_csv("data/StockData.csv",index=False)
